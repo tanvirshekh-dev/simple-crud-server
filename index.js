9c03a86dad68d65e09c522a62cc7335bb8de04dc
Tanvir Shekh <tanvirshekh.dev@gmail.com>
2026-04-02 22:27:00 +0600

    make users id are comments

--- index.js
+++ index.js
@@ -40,13 +40,13 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/users/:id', async (req, res) => {
-      const id = req.params.id;
-      console.log('need user id: ', id)
-      const query = { _id: new ObjectId(id) }
-      const result = await usersCollection.findOne(query);
-      res.send(result);
-    })
+    // app.get('/users/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   console.log('need user id: ', id)
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await usersCollection.findOne(query);
+    //   res.send(result);
+    // })
 
     // add database related apis here
     app.post('/users', async (req, res) => {
